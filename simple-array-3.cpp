f97abb8424212a7e1d7ebf294880f51c27b1f214
BrainThinker22 <gmpreslavcho@gmail.com>
2024-08-29 14:30:38 +0200

    Add files via upload

---
+++ simple-array-3.cpp
@@ -0,0 +1,22 @@
+// THIRD SIMPLE ARRAY CODE
+
+#include <iostream>
+
+int main() {
+
+    double prices[] = { 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10 };
+
+    std::cout << prices[0] << '\n';
+    std::cout << prices[1] << '\n';
+    std::cout << prices[2] << '\n';
+    std::cout << prices[3] << '\n';
+    std::cout << prices[4] << '\n';
+    std::cout << prices[5] << '\n';
+    std::cout << prices[6] << '\n';
+    std::cout << prices[7] << '\n';
+    std::cout << prices[8] << '\n';
+    std::cout << prices[9] << '\n';
+
+    return 0;
+
+}
