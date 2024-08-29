7aec74f4a8baaf3f3511cfb1934398bd18314aed
BrainThinker22 <gmpreslavcho@gmail.com>
2024-08-29 14:23:50 +0200

    Add files via upload

---
+++ arrays.cpp
@@ -0,0 +1,70 @@
+/*
+
+        // VERY SIMPLE ARRAY CODE
+
+#include <iostream>
+
+int main()
+{
+    std::string salad[3];
+
+    salad[0] = "tomato";
+    salad[1] = "cucumber";
+    salad[2] = "lettuce";
+
+    std::cout << salad[0] << '\n';
+    std::cout << salad[1] << '\n';
+    std::cout << salad[2] << '\n';
+
+    return 0;
+
+}
+
+*/
+
+/*
+
+        // ANOTHER VERY SIMPLE ARRAY CODE
+
+#include <iostream>
+
+    int main()
+    {
+        std::string salad[] = { "tomato", "cucumber", "lettuce" };
+
+        std::cout << salad[0] << '\n';
+        std::cout << salad[1] << '\n';
+        std::cout << salad[2] << '\n';
+
+        return 0;
+
+    }
+    
+    */
+
+/*
+
+        // THIRD SIMPLE ARRAY CODE
+
+#include <iostream>
+
+int main() {
+
+    double prices[] = { 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10 };
+
+    std::cout << prices[0] << '\n';
+    std::cout << prices[1] << '\n';
+    std::cout << prices[2] << '\n';
+    std::cout << prices[3] << '\n';
+    std::cout << prices[4] << '\n';
+    std::cout << prices[5] << '\n';
+    std::cout << prices[6] << '\n';
+    std::cout << prices[7] << '\n';
+    std::cout << prices[8] << '\n';
+    std::cout << prices[9] << '\n';
+
+    return 0;
+
+}
+
+*/
