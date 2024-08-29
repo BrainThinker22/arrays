039f6d694870656896ae1e58e92791c7f589c02c
BrainThinker22 <gmpreslavcho@gmail.com>
2024-08-29 14:27:38 +0200

    Add files via upload

---
+++ simple-array-2.cpp
@@ -0,0 +1,15 @@
+// ANOTHER VERY SIMPLE ARRAY CODE
+
+#include <iostream>
+
+int main()
+{
+    std::string salad[] = { "tomato", "cucumber", "lettuce" };
+
+    std::cout << salad[0] << '\n';
+    std::cout << salad[1] << '\n';
+    std::cout << salad[2] << '\n';
+
+    return 0;
+
+}
